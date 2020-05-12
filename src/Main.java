be97adf47ff102f70b9432072892925d59e4b2f2
Hollow-Serenity <dino.cosic1999@gmail.com>
2020-05-12 22:42:43 +0200

    Feature 2

--- src/Main.java
+++ src/Main.java
@@ -3,4 +3,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Test!");
     }
+
+    public void feature2()
+    {
+        System.out.println("Testing feature 2");
+    }
 }
