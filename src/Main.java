62163c61a36e99784b433bd5ce05aed826050e84
Jeremy-michiels <19014902@student.hhs.nl>
2020-05-15 20:00:32 +0200

    De code van de resolutie

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,47 @@
-public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Test!");
+import javafx.application.Application;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Stack;
+
+public class Main extends Application {
+
+    Stage window;
+    Scene scene1, scene2;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        window = primaryStage;
+        Label label1 = new Label("Resolutie 1");
+        Button button1 = new Button("Ga naar resolutie 2");
+        button1.setOnAction(e -> window.setScene(scene2));
+
+        VBox layout1 = new VBox(20);
+        layout1.getChildren().addAll(label1, button1);
+        scene1 = new Scene(layout1, 800, 600);
+
+        Button button2 = new Button("Ga naar resolutie 1");
+        button2.setOnAction(e -> window.setScene(scene1));
+
+        StackPane layout2 = new StackPane();
+        layout2.getChildren().add(button2);
+        scene2 = new Scene (layout2, 600, 300);
+
+        window.setScene(scene1);
+        window.setTitle("Titel");
+        window.show();
+
     }
 
 
-    public void feature1()
-    {
-        System.out.println("Dit is feature 1");
+    public static void main(String[] args) {
+        launch(args);
     }
 }
