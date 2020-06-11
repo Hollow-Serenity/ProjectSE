87ab67636bf9832f9c8a1dff7fab833186df6d9c
Sirvhh1 <19064373@student.hhs.nl>
2020-06-11 21:25:02 +0200

    Rekenopdrachten kunnen gemaakt worden en de berekeningen hebben de testcases gehaald.

--- src/Main/Question.java
+++ src/Main/Question.java
@@ -5,6 +5,7 @@ import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import java.util.Random;
 import java.util.ArrayList;
 
 public class Question {
@@ -20,6 +21,37 @@ public class Question {
     public Question() {
     }
 
+    public void makeMathExpression(){
+        Random rand = new Random();
+        final char[] OPERATOR_LIST = {'+','-','x','/'};
+        int getal1 = rand.nextInt(10)+1;
+        int getal2 = rand.nextInt(10)+1;
+        int getal3 = rand.nextInt(4);
+        if(getal3 == 3) {
+            getal1 = getal2*(rand.nextInt(11)+1);
+        }
+        if(getal2 > getal1) {
+            int temp = getal2;
+            getal2 = getal1;
+            getal1 = temp;
+        }
+        setText(getal1 + ""+OPERATOR_LIST[getal3]+ getal2);
+        setAnswer(String.valueOf(calculateMathExpression(getal1,getal2,OPERATOR_LIST[getal3])));
+    }
+    public static int calculateMathExpression(int getal1, int getal2, char operator) {
+        switch (operator) {
+            case '+':
+                return getal1 + getal2;
+            case '-':
+                return getal1 - getal2;
+            case 'x':
+                return getal1 * getal2;
+            case '/':
+                return getal1/getal2;
+            default:
+                return 0;
+        }
+    }
     public void setText(String text) {
         this.text = text;
     }
