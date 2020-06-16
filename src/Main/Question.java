49baaae2df2d4fe01a19337a9bc0ce0296dca0d2
Jeremy-michiels <19014902@student.hhs.nl>
2020-06-16 16:30:27 +0200

    Resolution for ReadingAssignment

--- src/Main/Question.java
+++ src/Main/Question.java
@@ -101,8 +101,8 @@ class ReadingAssignment extends Question {
 
 class LittleFoxStory extends ReadingAssignment {
     public LittleFoxStory() {
-        setStory("A fox has been in my yard. He wants to play. \n" +
-                "The fox gets on the log. Then he hides under the deck \n" +
+        setStory("A fox has been in my yard. \nHe wants to play. \n" +
+                "The fox gets on the log.\nThen he hides under the deck \n" +
                 "He is fun.");
         addQuestion(new Question("Where did the fox hide?", "Under the deck"));
         addQuestion(new Question("What does the fox get on?", "The log"));
@@ -112,11 +112,11 @@ class LittleFoxStory extends ReadingAssignment {
 
 class TheSnailStory extends ReadingAssignment {
     public TheSnailStory() {
-        setStory("Gail the snail was very sad. She felt like she was too plain. \n" +
+        setStory("Gail the snail was very sad.\nShe felt like she was too plain. \n" +
                 "'I know how to fix this!' she said in her brain. \n" +
-                "'I will stain my shell with paint! Gail did not want to wait. \n" +
-                "She got a pall and started to paint. Oh no! It started to rain. \n" +
-                "The paint washed off. Gail the snail was plain again!");
+                "'I will stain my shell with paint! \nGail did not want to wait. \n" +
+                "She got a pall and started to paint. \nOh no! It started to rain. \n" +
+                "The paint washed off. \nGail the snail was plain again!");
         addQuestion(new Question("Why was Gail sad?", "She felt like she was too plain"));
         addQuestion(new Question("Why did the paint wash of?", "It started to rain"));
         addQuestion(new Question("How did Gail fix her problem?", "She stained her shell with paint"));
@@ -125,21 +125,21 @@ class TheSnailStory extends ReadingAssignment {
 
 class TwoMiceStory extends ReadingAssignment {
     public TwoMiceStory() {
-        setStory("Mike and Moe were two mice. They lived in a little green house \n" +
-                "Mike and Moe saw some cheese. 'I want the cheese!' said Mike \n" +
-                "'It's mine!' said Moe. Mike and Moe yelled and yelled. \n" +
-                "'How about we split it?' said Moe. 'Yes!' said Mike. \n" +
+        setStory("Mike and Moe were two mice. \nThey lived in a little green house \n" +
+                "Mike and Moe saw some cheese. \n'I want the cheese!' said Mike \n" +
+                "'It's mine!' said Moe. \nMike and Moe yelled and yelled. \n" +
+                "'How about we split it?' said Moe. \n'Yes!' said Mike. \n" +
                 "They both had some cheese.");
-        addQuestion(new Question("What were the two mice called?", "Mike and Moe"));
-        addQuestion(new Question("What did the two mice both want?", "Cheese"));
-        addQuestion(new Question("Where did the two mice live?", "In a little green house"));
+        addQuestion(new Question("What were the two \nmice called?", "Mike and Moe"));
+        addQuestion(new Question("What did the two \nmice both want?", "Cheese"));
+        addQuestion(new Question("Where did the two \nmice live?", "In a little green house"));
     }
 }
 
 class PurpleMouseStory extends ReadingAssignment {
     public PurpleMouseStory() {
-        setStory("Poppy is a purple mouse, who lives in a big blue house. \n" +
-                "Poppy likes to go on walks if the weather is good, \n" +
+        setStory("Poppy is a purple mouse, \nwho lives in a big blue house. \n" +
+                "Poppy likes to go on walks \nif the weather is good, \n" +
                 "if not he stays at home and drinks some tea.");
         addQuestion(new Question("What animal is Poppy?", "A mouse"));
         addQuestion(new Question("Where does Poppy live?", "In a big blue house"));
