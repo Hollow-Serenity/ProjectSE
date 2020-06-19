5e05da5f70755071d63a09cbf9786faebb705a5a
Sirvhh1 <19064373@student.hhs.nl>
2020-06-19 18:13:47 +0200

    Tests helemaal af

--- test/Main/TestCalculations.java
+++ test/Main/TestCalculations.java
@@ -14,9 +14,19 @@ public class TestCalculations {
     }
 
     @Test
+    public void WrongTestPlus(){
+        assertEquals(14, Question.calculateMathExpression(12,6,'+'));
+        assertEquals(12228,Question.calculateMathExpression(657,1071,'+'));
+    }
+    @Test
     public void TestMinus(){
         assertEquals(2,Question.calculateMathExpression(9,7,'-'));
         assertEquals(9,Question.calculateMathExpression(100,91,'-'));
+    }
+    @Test
+    public void WrongTestMinus(){
+        assertEquals(700,Question.calculateMathExpression(856,5,'-'));
+        assertEquals(35124,Question.calculateMathExpression(154,1,'-'));
     }
 
     @Test
@@ -26,8 +36,20 @@ public class TestCalculations {
     }
 
     @Test
+    public void WrongTestDivision(){
+        assertEquals(9,Question.calculateMathExpression(180,45,'/'));
+        assertEquals(6,Question.calculateMathExpression(270,5,'/'));
+    }
+
+    @Test
     public void TestMultiplication(){
         assertEquals(80,Question.calculateMathExpression(10,8,'x'));
         assertEquals(36,Question.calculateMathExpression(12,3,'x'));
+    }
+
+    @Test
+    public void WrongTestMultiplication(){
+        assertEquals(64,Question.calculateMathExpression(9,8,'x'));
+        assertEquals(21,Question.calculateMathExpression(1322,56,'x'));
     }
 }
